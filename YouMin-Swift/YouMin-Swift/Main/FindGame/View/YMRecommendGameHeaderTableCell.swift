d4947ef961bdb2a59162d469b4c7d68fb5786d34
admin <zuoweijie2017@gmail.com>
2021-05-11 20:10:36 +0800

    增加推荐游戏卡片

---
+++ YouMin-Swift/YouMin-Swift/Main/FindGame/View/YMRecommendGameHeaderTableCell.swift
@@ -0,0 +1,16 @@
+//
+//  YMRecommendGameHeaderTableCell.swift
+//  YouMin-Swift
+//
+//  Created by admin on 2021/5/11.
+//
+
+import UIKit
+
+class YMRecommendGameHeaderTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+}
---
+++ YouMin-Swift/YouMin-Swift/Main/FindGame/View/YMRecommendGameTableCell.swift
@@ -0,0 +1,17 @@
+//
+//  YMRecommendGameTableCell.swift
+//  YouMin-Swift
+//
+//  Created by admin on 2021/5/11.
+//  推荐游戏卡片 推荐游戏卡片
+
+import UIKit
+
+class YMRecommendGameTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+}
